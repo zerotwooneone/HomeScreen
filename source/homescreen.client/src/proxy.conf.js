fef3645d4353839203cf660a9df9a80e673a08f2
zero <squiredd@gmail.com>
2024-12-15 17:02:19 -0800

    initial

---
+++ source/homescreen.client/src/proxy.conf.js
@@ -0,0 +1,17 @@
+const { env } = require('process');
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:44396'; //was 7190
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/weatherforecast",
+    ],
+    target,
+    secure: false,
+    changeOrigin: true,
+  }
+]
+
+module.exports = PROXY_CONFIG;
