e25fb2a9e19b303e97f2283edb5828b30643700c
zero <squiredd@gmail.com>
2024-12-17 18:34:43 -0800

    fix websocket config

--- source/homescreen.client/src/proxy.conf.js
+++ source/homescreen.client/src/proxy.conf.js
@@ -1,17 +1,26 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7190';
+const lastResortPort = 7190;
+
+const httpTarget = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : `https://localhost:${lastResortPort}`;
 
 const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
-      "/screenHub",
     ],
-    target,
+    target: httpTarget,
     secure: false,
     changeOrigin: true,
+  },
+  {
+    context: [
+      "/screenHub",
+    ],
+    target: httpTarget,
+    secure: false,
+    "ws": true,
   }
 ]
 
