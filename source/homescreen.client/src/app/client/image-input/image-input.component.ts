4396a20324048e1bdc08b11164d6b69b563c5846
zero <squiredd@gmail.com>
2024-12-31 17:30:50 -0800

    add async img load validation

--- source/homescreen.client/src/app/client/image-input/image-input.component.ts
+++ source/homescreen.client/src/app/client/image-input/image-input.component.ts
@@ -1,6 +1,7 @@
 import {Component, input, OnInit, signal} from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { debounceTime, map, merge, of, Subject, take, timeout } from 'rxjs';
+import {AbstractControl, FormControl} from '@angular/forms';
+import {BehaviorSubject, debounceTime, first,
+  firstValueFrom, map, merge, Observable, of, shareReplay, Subject, switchMap, take, timeout } from 'rxjs';
 
 @Component({
   selector: 'zh-image-input',
@@ -8,18 +9,30 @@ import { debounceTime, map, merge, of, Subject, take, timeout } from 'rxjs';
   styleUrl: './image-input.component.scss'
 })
 export class ImageInputComponent implements OnInit {
-   control= input.required<FormControl>();
-   timeoutMs = input(5000);
-   imageUrl = '';
-   private readonly imgErrorSubject = new Subject<Event>();
-   private readonly imgLoadSubject = new Subject<Event>();
-   private readonly imgAbortSubject = new Subject<Event>();
+  control= input.required<FormControl>();
+  timeoutMs = input(5000);
+  imageUrl = '';
+  private readonly imgErrorSubject = new Subject<Event>();
+  private readonly imgLoadSubject = new Subject<Event>();
+  private readonly imgAbortSubject = new Subject<Event>();
   imgVisible = signal<boolean>(false);
+  imgStatus = new BehaviorSubject<Observable<boolean>>(new BehaviorSubject(false));
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.control().addAsyncValidators([
+      (control: AbstractControl) => {
+        return firstValueFrom(this.imgStatus.pipe(
+          switchMap(obs=>obs),
+          map(status=>
+            status
+              ? null
+              : {'imageFailedToLoad': true})
+        ));
+      }
+    ])
     this.control().valueChanges
       .pipe(
         debounceTime(300)
@@ -28,25 +41,35 @@ export class ImageInputComponent implements OnInit {
         if(!value?.trim()){
           return;
         }
+        const timeoutMs = this.timeoutMs()<0
+          ? 1
+          : this.timeoutMs();
+
         this.imageUrl ="";
         this.imgVisible.set(false);
-        merge(
+        let imageLoadSuccess = merge(
           this.imgErrorSubject.pipe(
-            take(1),
-            map(()=>false)),
+            map(()=> {
+              console.warn('image failed to load');
+              return false;
+            })),
           this.imgLoadSubject.pipe(
-            take(1),
-            map(()=>true)),
+            map(()=> {
+              console.info('image loaded');
+              return true;
+            })),
           this.imgAbortSubject.pipe(
-            take(1),
             map(()=>false))
         ).pipe(
           take(1),
           timeout({
-            first: this.timeoutMs(),
+            first: timeoutMs,
             with: ()=>of(false)
           })
-        ).subscribe(success=>{
+          //,shareReplay(1)
+        );
+        this.imgStatus.next(imageLoadSuccess);
+        imageLoadSuccess.subscribe(success=>{
           this.imgVisible.set(success);
         });
         this.imageUrl = value;
