d43b133f8bb0d88bb43586839b616dfc5b1d8a8d
zero <squiredd@gmail.com>
2025-02-10 19:32:58 -0600

    cleanup

--- source/homescreen.client/src/app/screen/screen.module.ts
+++ source/homescreen.client/src/app/screen/screen.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { ScreenRoutingModule } from './screen-routing.module';
 import {ScreenHubService} from "./screen-hub.service";
-import {ScreenHomeComponent} from "./screen-home/screen-home.component";
 
 @NgModule({
   imports: [
